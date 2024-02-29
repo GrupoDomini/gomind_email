b35e4581c67404f1fc745bdf89efbc851de11616
GDJeffersonCarvalho <jefferson.carvalho@grupodomini.com>
2024-02-29 17:04:54 -0300

    feat: update setup file with github url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="gomind_email",
     version="0.0.1",
     description="GoMind email sender service",
-    url="",
+    url="https://github.com/GrupoDomini/gomind_email.git",
     author="JeffersonCarvalhoGD",
     author_email="jefferson.carvalho@grupodomini.com",
     license="unlicense",
