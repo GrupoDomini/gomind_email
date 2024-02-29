96b9023c18c51a06014de19942c3ce07c39981f6
GDJeffersonCarvalho <jefferson.carvalho@grupodomini.com>
2024-02-29 17:11:33 -0300

    feat: update setup py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name="gomind_email",
+    python_requires=">=3.6",
     version="0.0.1",
     description="GoMind email sender service",
     url="https://github.com/GrupoDomini/gomind_email.git",
@@ -10,4 +11,5 @@ setup(
     license="unlicense",
     packages=["gomind_email"],
     zip_safe=False,
+    install_requires=["get-mac==0.9.2"],
 )
