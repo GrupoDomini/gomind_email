52e7b3a0e24dbd8b1d2b322f751cfcccc75a8aca
Mayara Nascimento <mayara_silvarp@hotmail.com>
2024-07-30 16:44:06 -0300

    feat: Update email functionality with image attachments

--- gomind_email/__init__.py
+++ gomind_email/__init__.py
@@ -10,6 +10,7 @@ from email.mime.base import MIMEBase
 from email import encoders
 import smtplib
 from datetime import datetime
+from typing import Literal
 
 
 try:
@@ -23,43 +24,165 @@ except Exception as e:
 def enviar_email(
     msg_mail_to,
     msg_mail_cc="",
-    result=True,
+    result=Literal['start', 'aguarda_analise', 'sucesso', 'erro'],
     RPA="[Robô ainda não identificado]",
 ):
     try:
         """Corpo do e-mail"""
+
+        start = f"""
+        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+            <tr>
+                <td bgcolor="#e8e8e8" style="padding: 10px 0 10px 0;">
+                    <table align="center" border="0" cellpadding="0" cellspacing="0" width="700" style="border-collapse: collapse;">
+                        
+                        <tr>
+                            <td bgcolor="#ffffff">
+                                <table border="0" cellpadding="0" cellspacing="0" width="100%">
+
+                                    <tr>
+                                        <td style="color: #153643; font-family: Arial, sans-serif; font-size: 24px; text-align: center; padding-top: 50px">
+                                            <b>Olá! Esta é uma mensagem automática da Mia.</b>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td style="padding: 20px 0 30px 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 25px; text-align: center;">
+                                            Identificamos a inicialização do <b>{RPA}</b> e a execução está em andamento.
+                                            <br/>
+                                            Retornaremos com o resultado em breve.
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <a href="https://www.gomind.com.br">
+                                                <img src="https://github.com/GrupoDomini/Public/blob/main/ASS_MIA.png?raw=true" alt="Go Mind" width="70%" height="auto" style="display: block; margin: auto;" />
+                                            </a>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+        """
+
+        aguarda_analise = f"""
+        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+            <tr>
+                <td bgcolor="#e8e8e8" style="padding: 10px 0 10px 0;">
+                    <table align="center" border="0" cellpadding="0" cellspacing="0" width="700" style="border-collapse: collapse;">
+                        
+                        <tr>
+                            <td bgcolor="#ffffff">
+                                <table border="0" cellpadding="0" cellspacing="0" width="100%">
+
+                                    <tr>
+                                        <td style="color: #153643; font-family: Arial, sans-serif; font-size: 24px; text-align: center; padding-top: 50px">
+                                            <b>Olá! Esta é uma mensagem automática da Mia.</b>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td style="padding: 20px 0 30px 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 25px; text-align: center;">
+                                            O <b>{RPA}</b> já coletou os documentos e agora
+                                            <br/>
+                                            aguarda a sua análise para seguir com o processamento!<br/>
+                                            Clique <a style="color: #0D2B5B;" href="https://portalmia.app"><b>aqui</b></a> para acessar a plataforma MIA.
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <a href="https://www.gomind.com.br">
+                                                <img src="https://github.com/GrupoDomini/Public/blob/main/ASS_MIA.png?raw=true" alt="Go Mind" width="70%" height="auto" style="display: block; margin: auto;" />
+                                            </a>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+        """
+
         sucesso = f"""
-        <center>
-        <p>Prezados,</p>
-        <p><i>Esta é uma mensagem automática da <b>Mia.</b></i></p>
-        
-        <p>Registramos a conclusão do {RPA} com sucesso! Verifique o resultado do processo em anexo.</p>
+        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+            <tr>
+                <td bgcolor="#e8e8e8" style="padding: 10px 0 10px 0;">
+                    <table align="center" border="0" cellpadding="0" cellspacing="0" width="700" style="border-collapse: collapse;">
+                        
+                        <tr>
+                            <td bgcolor="#ffffff">
+                                <table border="0" cellpadding="0" cellspacing="0" width="100%">
 
-        <p>
-            <img src="https://raw.githubusercontent.com/GrupoDomini/Public/main/mail_suc.png"></img>
-        </p>
-
-        Att, <b>Mia - Go Mind</b><br>
-        Departamento de Tecnologia<br>
-        <a href='https://www.gomind.com.br' title='Unlimited Growth'>www.gomind.com.br</a>
-        </center>
+                                    <tr>
+                                        <td style="color: #153643; font-family: Arial, sans-serif; font-size: 24px; text-align: center; padding-top: 50px">
+                                            <b>Olá! Esta é uma mensagem automática da Mia.</b>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td style="padding: 20px 0 30px 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 25px; text-align: center;">
+                                            Registramos a conclusão do <b>{RPA}</b> com sucesso!.
+                                            <br/>
+                                            Clique <a style="color: #0D2B5B;" href="https://portalmia.app"><b>aqui</b></a> e verifique os resultados
+                                            <br/>
+                                            <b style="color: #13B2A3;">na plataforma MIA</b>.
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <a href="https://www.gomind.com.br">
+                                                <img src="https://github.com/GrupoDomini/Public/blob/main/ASS_MIA.png?raw=true" alt="Go Mind" width="70%" height="auto" style="display: block; margin: auto;" />
+                                            </a>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
         """
 
         erro = f"""
-        <center>
-        <p>Prezados,</p>
-        <p><i>Esta é uma mensagem automática da <b>Mia.</b></i></p>
-        Registramos erro durante execução do {RPA}.<br>
-        <p>Código de Referência: gosppt{mac_address}.</p>
-        
-        <p>
-            <img src="https://raw.githubusercontent.com/GrupoDomini/Public/main/mail_erro.png"></img>
-        </p>
+        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+            <tr>
+                <td bgcolor="#e8e8e8" style="padding: 10px 0 10px 0;">
+                    <table align="center" border="0" cellpadding="0" cellspacing="0" width="700" style="border-collapse: collapse;">
+                        
+                        <tr>
+                            <td bgcolor="#ffffff">
+                                <table border="0" cellpadding="0" cellspacing="0" width="100%">
 
-        Att, <b>Mia - Go Mind</b><br>
-        Departamento de Tecnologia<br>
-        <a href='https://www.gomind.com.br' title='Unlimited Growth'>www.gomind.com.br</a>
-        </center>
+                                    <tr>
+                                        <td style="color: #153643; font-family: Arial, sans-serif; font-size: 24px; text-align: center; padding-top: 50px">
+                                            <b>Olá! Esta é uma mensagem automática da Mia.</b>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td style="padding: 20px 0 30px 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 25px; text-align: center;">
+                                            Registramos erro durante execução do <b>{RPA}</b>.
+                                            <br/>
+                                            Código de Referência: <b style="color: #13B2A3;">gosppt{mac_address}</b>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <a href="https://www.gomind.com.br">
+                                                <img src="https://github.com/GrupoDomini/Public/blob/main/ASS_MIA.png?raw=true" alt="Go Mind" width="60%" height="auto" style="display: block; margin: auto;" />
+                                            </a>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
         """
 
         # Endereços
@@ -69,31 +192,24 @@ def enviar_email(
         msg["Cc"] = msg_mail_cc
 
         # Assunto da mensagem
-        if result:
+        if result == 'sucesso':
             msg["Subject"] = f"Go Mind | Informativo RPA '{RPA}'- Sucesso"
             msg.attach(MIMEText(sucesso, "html"))
+        elif result == 'start':
+            msg["Subject"] = f"Go Mind | Informativo RPA '{RPA}'- Inicializou"
+            msg.attach(MIMEText(start, "html"))
+        elif result == 'aguarda_analise':
+            msg["Subject"] = f"Go Mind | Informativo RPA '{RPA}'- Aguardando Análise"
+            msg.attach(MIMEText(aguarda_analise, "html"))
         else:
             msg["Cco"] = "suporte@gomind.com.br"
-            msg["Subject"] = f"Go Mind | Informativo RPA '{RPA}' - Erro"
+            msg["Subject"] = f"Go Mind | Informativo RPA '{RPA}'- Erro"
             msg.attach(MIMEText(erro, "html"))
 
-        # Anexo
-        # if result:
-        #     attachment = open(file_path, "rb")  # Local do arquivo
-        #     anexo = "RPA_resultado.xlsx"  # Nome e extensão do arquivo em anexo
 
-        #     part = MIMEBase("application", "octet-stream")
-        #     part.set_payload((attachment).read())
-        #     encoders.encode_base64(part)
-        #     part.add_header("Content-Disposition", "attachment; filename= %s" % anexo)
-        #     msg.attach(part)
-        #     attachment.close()
         text = msg.as_string().encode(encoding="latin-1", errors="strict")
 
         """Protocolo & segurança"""
-        # Versão Gmail
-        # s = smtplib.SMTP('smtp.gmail.com: 587') #Gmail Server
-        # password = "pusuxxkylregxdis" #token app gerada na conta Google (john.rpa.domini@gmail.com)
 
         # Versão Microsoft
         s = smtplib.SMTP("smtp.office365.com: 587")  # Office365 Server
@@ -103,8 +219,21 @@ def enviar_email(
 
         # Envio da mensagem
         s.login(msg["From"], password)
-        if result:
+        if result == 'start':
             s.sendmail(msg["From"], msg["To"].split(",") + msg["Cc"].split(","), text)
+
+        elif result == 'aguarda_analise':
+            s.sendmail(
+                msg["From"],
+                msg["To"].split(",") + msg["Cc"].split(","),
+                text,
+            )
+        elif result == 'sucesso':
+            s.sendmail(
+                msg["From"],
+                msg["To"].split(",") + msg["Cc"].split(","),
+                text,
+            )
         else:
             s.sendmail(
                 msg["From"],
@@ -181,3 +310,4 @@ def log_email(
         except Exception as e:
             print(e)
             print("Erro ao enviar e-mail")
+
